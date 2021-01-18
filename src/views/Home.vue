bb6a9d45ac3f493bdb2b0db8542c170b784c4d65
Stefan <ststroppel@googlemail.com>
2021-01-18 19:33:19 +0100

    fix eslint issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-input placeholder="Please input" v-model="input"></el-input>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      input: ''
+    };
   }
 })
 export default class Home extends Vue {}
